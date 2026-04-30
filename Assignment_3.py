62ce02e91a095d98a1d1cdcef6cef3a284429023
Patrick Burzynski <s399755@students.cdu.edu.au>
2026-04-30 18:06:00 +1000

    Initial upload of setup
    Initial setup of program structure. Created basic Tkinter window with title, size and main loop.

---
+++ Assignment_3.py
@@ -0,0 +1,22 @@
+# Group Name: [DAN/EXT 73]
+
+# Group Member:
+# Patrick Burzynski - [399755]
+
+# Assignment 3
+"""This program creates a desktop application using Tkinter and OpenCV. 
+It allows the user to load an image, generates random differences,
+and lets the user find those differences by clicking on the image."""
+
+import tkinter as tk
+
+# Create window
+root = tk.Tk()
+root.title("Find the Differences")
+root.geometry("800*600")
+
+# run program
+root.mainloop()
+
+
+
