f733ac51ac03b25a2e25c44e98756c8dd7cd7ecc
Patrick Burzynski <s399755@students.cdu.edu.au>
2026-05-03 18:39:59 +1000

    Update Assignment_3.py
    Update 6 - Added ImageProcessor class to load images using OpenCV. Implemented basic image loading functionality and connected it to GUI button.

--- Assignment_3.py
+++ Assignment_3.py
@@ -10,22 +10,42 @@ and lets the user find those differences by clicking on the image."""
 
 import tkinter as tk
 from tkinter import filedialog 
+import cv2
 
+#image processor 
+class ImageProcessor:
+    def __init__(self):
+        self.original_image = None
+        self.modified_image = None
+
+    def load_image(self,path):
+        self.orginal_image = cv2.imread(path)
+
+        if self.original_image is None:
+            print("Error loading image")
+            return False
+
+        self.modified_image = self.original_image.copy()
+        print("Image copied for modification")
+        return True
+           
+       
 #Main Class
 class GameApp:
    def __init__(self, root):
-       self.root = tk.Tk()
+       self.root = root
+       self.processor = ImageProcessor()
       
        # window setup
        self.root.title("Find the Differences")
-       self.root.geometry("800*600")
+       self.root.geometry("800x600")
 
        # Label
        self.label_info = tk.Label(root, text="Load an image to start")
        self.label_info.pack()
 
        #Buttons
-       self.btn_load + tk.Button(root, text="Load Image")
+       self.btn_load = tk.Button(root, text="Load Image", command=self.load_image)
        self.btn_load.pack()
 
        self.btn_reveal = tk.Button(root, text="Reveal Differences")
@@ -35,12 +55,16 @@ class GameApp:
        self.canvas = tk.Canvas(root, width=400, height=400, bg="grey")
        self.anvas.pack()   
 
-#Load image
+#Load image 
 def load_image(self):
     file_path = filedialog.askopenfilename()
     print(file_path)
 
-self.btn_load = tk.Button(root,text="Load Image", command=self.load_image)   
+    if file_path:
+        self.processor.load_image(file_path)
+
+        if success:
+            print("Image loaded successfully")
 
 # run program
 root = tk.Tk()
